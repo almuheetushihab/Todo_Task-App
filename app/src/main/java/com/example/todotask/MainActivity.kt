0e09958071be5511fee9e4d48e618dc3107f158d
Almuheetu shihab <almuheetu@gmail.com>
2024-08-09 14:57:22 +0600

    set toolber in app

--- app/src/main/java/com/example/todotask/MainActivity.kt
+++ app/src/main/java/com/example/todotask/MainActivity.kt
@@ -6,9 +6,11 @@ import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.todotask.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
 
@@ -20,6 +22,15 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        
+
 
     }
 }
